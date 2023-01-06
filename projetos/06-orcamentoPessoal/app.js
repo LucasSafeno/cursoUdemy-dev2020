bd3c837b6c6d9dadf6b465b57dc8ca825211af46
Lucas Safeno <lugnorio@gmail.com>
2023-01-06 00:35:54 -0300

    JavaScript : App Orçamento Pessoal - Listando despesas parte 2

--- projetos/06-orcamentoPessoal/app.js
+++ projetos/06-orcamentoPessoal/app.js
@@ -49,16 +49,19 @@ class BD{
     recuperarTodosRegistros(){
         let despesas = Array()
 
-        let id = JSON.parse(localStorage.getItem('id'))
+        let id = localStorage.getItem('id')
 
         //recuperar todas as despesas registradas
         for(let i = 1; i <= id; i++){
-            let despesa = localStorage.getItem(i)
+            let despesa = JSON.parse(localStorage.getItem(i))
+            
             if(despesa === null){
                 continue
             }
             despesas.push(despesa)
         }
+
+        return despesas
     }
 
 
@@ -104,8 +107,42 @@ function cadastrarDespesa(){
 }//cadastrarDespesa
 
 function carregaListaDespesas(){
-    let despesas = Array()
     
+    let despesas = Array()
     despesas = bd.recuperarTodosRegistros()
+
+    var listaDespesas = document.getElementById('listaDespesas')
+    
+
+    despesas.forEach(function(d){
+        let linha = listaDespesas.insertRow()
+
+        linha.insertCell(0).innerHTML = `${ d.dia}/${d.mes}/${d.ano}`
+        //Ajustar o tipo
+        switch(parseInt(d.tipo)){
+            case 1: d.tipo = 'Alimentação'
+                break
+            case 2: d.tipo = 'Educação'
+                break
+            case 3: d.tipo = 'Lazer'
+                break
+            case 4: d.tipo = 'Saúde'
+                break
+            case 5: d.tipo = 'Transporte'
+                break
+
+        }
+        linha.insertCell(1).innerHTML = d.tipo
+        linha.insertCell(2).innerHTML = d.descricao
+        linha.insertCell(3).innerHTML = d.valor
+        
+    })
+
+  
+
+   
+
+    
+
 } //carregaListaDespesas
 
