8b8430a31385208eacdbdedeb16789767a549040
Lucas Safeno <lugnorio@gmail.com>
2023-01-09 12:54:23 -0300

    357. App Help Desk - Registrando chamados

---
+++ projetos/07-appHelpDesk/registra_chamado.php
@@ -0,0 +1,18 @@
+<?php
+
+// Montando texto
+$titulo = str_replace('#', '-', $_POST['titulo']);
+$categoria = str_replace('#', '-', $_POST['categoria']);
+$descricao = str_replace('#', '-', $_POST['descricao']);
+
+$texto = $titulo. '#' .$categoria. '#' . $descricao . PHP_EOL;
+
+// Abrindo arquivo
+$arquivo = fopen('arquivo.hd', 'a');
+// Escrevendo texto
+fwrite($arquivo, $texto);
+// Fechadno arquivo
+fclose($arquivo);
+
+header("Location: abrir_chamado.php");
+?>
