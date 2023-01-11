f020ba5d0d496e57413d99b316e682b8dcad2294
Lucas Safeno <lugnorio@gmail.com>
2023-01-11 01:15:02 -0300

    444 - Tratando exceptions (PDOException)

--- 13-php-pdo/443-criando-conexao/index.php
+++ 13-php-pdo/443-criando-conexao/index.php
@@ -1,6 +1,11 @@
 <?php
 $dsn    = 'mysql:host=localhost;dbname=php_pdo';
 $user   = 'root';
-$pass = 'root';
+$pass = 'roots';
 
-$conexao = new PDO($dsn, $user, $pass);
+try{
+    $conexao = new PDO($dsn, $user, $pass);
+}catch(PDOException $e){
+    echo "Erro" . $e->getCode() . ' Mensagem ' . $e->getMessage();
+}
+
