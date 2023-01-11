b5c54ec5da2ef362e05a1d2cf570910aed758d12
Lucas Safeno <lugnorio@gmail.com>
2023-01-11 15:01:11 -0300

    460 - App Lista de Tarefas - Listando todos os registros parte 1

--- projetos/10-appListaTarefas/tarefa_controller.php
+++ projetos/10-appListaTarefas/tarefa_controller.php
@@ -3,12 +3,19 @@ require("tarefa.model.php");
 require("tarefa.service.php");
 require("conexao.php");
 
+$acao = isset($_GET['acao']) ?  $_GET['acao'] : $acao;
 
-$tarefa = new Tarefa();
-$tarefa->__set('tarefa', $_POST['tarefa']);
- 
-$conexao = new Conexao();
+if($acao == 'inserir'){
+    $tarefa = new Tarefa();
+    $tarefa->__set('tarefa', $_POST['tarefa']);
+    
+    $conexao = new Conexao();
 
-$tarefaService = new TarefaService($conexao,$tarefa);
-$tarefaService->inserir();
-header('Location: nova_tarefa.php?inclusao=1');
+    $tarefaService = new TarefaService($conexao,$tarefa);
+    $tarefaService->inserir();
+    header('Location: nova_tarefa.php?inclusao=1');
+}else if($acao == 'recuperar'){
+    echo 'Chegamos até aqui';
+}
+
+
