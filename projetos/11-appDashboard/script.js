3349932262a6447abe2861e5c232e1da7a735fdb
Lucas Safeno <lugnorio@gmail.com>
2023-01-17 15:31:12 -0300

    502 - Projeto Dashboard - Jquery

--- projetos/11-appDashboard/script.js
+++ projetos/11-appDashboard/script.js
@@ -1,3 +1,15 @@
 $(document).ready(() => {
-	
+
+    $('#documentacao').on('click', () =>{
+       // $('#pagina').load('documentacao.html')
+       $.get('documentacao.html', data =>{
+            $('#pagina').html(data)
+       }) // get
+
+
+    })
+    
+    $.post('suporte.html', data =>{
+        $('#pagina').html(data)
+   }) // get
 })
